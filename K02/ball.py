f2b99d7ece67bd9b6cd600b51b82b84e52a1bb43
Mario Filgueiras <magallanesdeveloper@gmail.com>
2021-04-27 23:23:22 +0200

    K02 - Con dos pelotas

--- K02/ball.py
+++ K02/ball.py
@@ -1,5 +1,19 @@
 import pygame as pg
 import sys
+from random import randint
+
+def rebotaX(x):
+    if x <= 0 or x >= ANCHO:
+        return -1  # cambiamos el signo
+    else:
+        return 1
+
+def rebotaY(y):
+    if y <= 0 or y >= ALTO:
+        return -1
+    
+    return 1  # nos podemos ahorrar el ELSE
+
 
 ROJO = (255, 0, 0)
 AZUL = (0, 0, 255)
@@ -10,14 +24,22 @@ ALTO = 600
 
 pg.init()
 pantalla = pg.display.set_mode((ANCHO, ALTO))
+reloj = pg.time.Clock()
 
-game_over = False
+# Bola 1
 x = ANCHO//2 # para que no tenga decimales
 y = ALTO//2
 vx = -8
 vy = -8
-reloj = pg.time.Clock()
 
+# Bola 2
+x2 = randint(0, ANCHO)
+y2 = randint(0, ALTO)
+vx2 = randint(5, 15)
+vy2 = randint(5, 15)
+
+
+game_over = False
 while not game_over:
     reloj.tick(60)  # por si queremos reducir la velocidad de la pelota
     # gestión de eventos
@@ -28,14 +50,17 @@ while not game_over:
     # gestión de la pantalla
     pantalla.fill(NEGRO)
     pg.draw.circle(pantalla, ROJO, (x, y), 10)
+    pg.draw.circle(pantalla, VERDE, (x2, y2), 10)
+
     x += vx
     y += vy
+    x2 += vx2
+    y2 += vy2
 
-    if y <= 0 or y>= ALTO:
-        vy = -vy
-    
-    if x <= 0 or x>= ANCHO:
-        vx = -vx
+    vy *= rebotaY(y)
+    vx *= rebotaX(x)
+    vy2 *= rebotaY(y2)
+    vx2 *= rebotaX(x2)
 
     # refrescamos pantalla
     pg.display.flip()
