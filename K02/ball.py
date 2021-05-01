f4d0ccde550f516b806618c781c169717a49f838
Mario Filgueiras <magallanesdeveloper@gmail.com>
2021-05-01 17:57:31 +0200

    K02 - Solución Mario (vidas, puntos y game over)

--- K02/ball.py
+++ K02/ball.py
@@ -13,9 +13,12 @@ pg.init()
 pantalla = pg.display.set_mode((ANCHO, ALTO))
 reloj = pg.time.Clock()
 
+fuente1 = pg.font.Font(None, 30)
+fuente2 = pg.font.Font(None, 60)
+
 
 class Bola():
-    def __init__(self, x, y, vx, vy, color, radio=10):
+    def __init__(self, x, y, vx, vy, color, radio=10, contadorColision=0):
         self.x = x
         self.y = y
         self.vx = vx
@@ -23,6 +26,7 @@ class Bola():
         self.color = color
         self.anchura = radio*2
         self.altura = radio*2
+        self.contadorColision = contadorColision
 
     def actualizar(self):
         self.x += self.vx
@@ -53,7 +57,8 @@ class Bola():
             self.y+self.anchura >= objeto.y and self.y+self.anchura <= objeto.y + objeto.anchura
 
         if choqueX and choqueY:  # para que haya colisión los dos choques tienen que ser True
-            self.vy *= -1            
+            self.vy *= -1
+            self.contadorColision += 5           
         
 
 class Raqueta():
@@ -78,7 +83,8 @@ class Raqueta():
             raqueta.x += raqueta.vx
 
 
-vidas = 3
+
+vidas = 3  # vidas al empezar el juego
 
 raqueta = Raqueta()
 
@@ -87,7 +93,8 @@ bola = Bola(randint(0, ANCHO),
             choice([randint(-10,-5), randint(5, 10)]),
             choice([randint(-10,-5), randint(5, 10)]),
             (randint(0,255), randint(0,255), randint(0,255)))
-    
+
+   
 
 game_over = False
 while not game_over and vidas > 0:
@@ -97,24 +104,41 @@ while not game_over and vidas > 0:
         if evento.type == pg.QUIT:
             game_over = True
 
-
     # modificación de estado
     raqueta.actualizar()
     pierdebola = bola.actualizar()
     if pierdebola:
         vidas -= 1
+    strVidas = str(vidas)
     
     bola.comprueba_colision(raqueta)
-        
+    strPuntos = str(bola.contadorColision)
+
+    texto1 = fuente1.render("Vidas:  {}".format(strVidas), True, (255, 255, 255))
+    texto2 = fuente1.render("Puntos:  {}".format(strPuntos), True, (255, 255, 255))
+      
     # gestión de la pantalla
     pantalla.fill(NEGRO)
-    bola.dibujar(pantalla)
-    raqueta.dibujar(pantalla)
-        
+    if vidas>0:
+        bola.dibujar(pantalla)
+        raqueta.dibujar(pantalla)
+    pantalla.blit(texto1, (30,10))
+    pantalla.blit(texto2, (650,10))
+
+
     # refrescamos pantalla
     pg.display.flip()
     if pierdebola:
         pg.time.delay(500)
+
+
+pg.time.delay(3000)  # nos permite ver la puntuación obtenida    
+
+pantalla.fill(NEGRO)
+texto3 = fuente2.render("GAME OVER", True, (255, 255, 255))
+pantalla.blit(texto3, (ANCHO//2 - texto3.get_width()//2, ALTO//2 - texto3.get_height()//2))
+pg.display.flip()
+pg.time.delay(3000)  # para que no desaparezca el GAME OVER inmediatamente
 
 pg.quit()
 sys.exit()
