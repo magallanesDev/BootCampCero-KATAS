5751022b6dfa846e275434c627b94ce6a6f98a27
Mario Filgueiras <magallanesdeveloper@gmail.com>
2021-05-05 23:02:21 +0200

    K02 - Raqueta eléctrica lista con 1 bola

--- K02/ball-2.py
+++ K02/ball-2.py
@@ -3,17 +3,47 @@ import sys
 import random
 ANCHO = 800
 ALTO = 600
-FPS = 60
+FPS = 50
 
-class Marcador():
+class Marcador(pg.sprite.Sprite):
     def __init__(self, x, y, fontsize=25, color=(255,255,255)):
+        super().__init__()
         self.fuente = pg.font.SysFont("Arial", fontsize)
-        self.x = x
-        self.y = y
+        self.text = 0
         self.color = color
-    def dibuja(self, text, lienzo):
-        image = self.fuente.render(str(text), True, self.color)
-        lienzo.blit(image, (self.x, self.y))
+        self.image = self.fuente.render(str(self.text), True, self.color)
+        self.rect = self.image.get_rect(topleft=(x,y))   
+    def update(self):
+        self.image = self.fuente.render(str(self.text), True, self.color)
+
+class Raqueta(pg.sprite.Sprite):
+    disfraces = ['electric00.png', 'electric01.png', 'electric02.png']
+    def __init__(self, x, y):
+        super().__init__()
+        self.imagenes = self.cargaImagenes()
+        self.imagen_actual = 0  # imagen en primera posición en la lista
+        self.image = self.imagenes[self.imagen_actual]
+        self.rect = self.image.get_rect(centerx = x, bottom = y)
+        self.vx = 7
+    def cargaImagenes(self):
+        imagenes = []  # variable local, sólo de este método. Es una lista vacia
+        for fichero in self.disfraces:
+            imagenes.append(pg.image.load("./images/{}".format(fichero)))
+        return imagenes
+    def update(self):
+        teclas_pulsadas = pg.key.get_pressed()
+        if teclas_pulsadas[pg.K_LEFT]:
+            self.rect.x -= self.vx
+        if teclas_pulsadas[pg.K_RIGHT]:
+            self.rect.x += self.vx
+        if self.rect.left <= 0:
+            self.rect.left = 0
+        if self.rect.right >= ANCHO:
+            self.rect.right = ANCHO
+        self.imagen_actual += 1  # pasamos a la siguiente imagen
+        if self.imagen_actual >= len(self.disfraces):
+            self.imagen_actual = 0  # volvemos a la primera imagen
+        self.image = self.imagenes[self.imagen_actual]
 
 class Bola(pg.sprite.Sprite):  # heredamos de la clase Sprite
     def __init__(self, x, y):
@@ -21,16 +51,12 @@ class Bola(pg.sprite.Sprite):  # heredamos de la clase Sprite
         super().__init__()  # otra forma de hacer lo de la línea de arriba
         self.image = pg.image.load('./images/ball1.png').convert_alpha()
         self.rect = self.image.get_rect(center=(x,y))  # el get_rect() nos da el rectángulo 30x30 (tamaño imagen) que envuelve la imagen y lo centra en (x,y)
-
         # tanto image como rect actúan como dos instancias
-
         self.vx = random.randint(5, 10) * random.choice([-1, 1])
         self.vy = random.randint(5, 10) * random.choice([-1, 1])
-
     def update(self):
         self.rect.x += self.vx
         self.rect.y += self.vy
-
         if self.rect.left <= 0 or self.rect.right >= ANCHO:
             self.vx *= -1
         if self.rect.top <= 0 or self.rect.bottom >= ALTO:
@@ -40,37 +66,32 @@ class Game():
     def __init__(self):
         self.pantalla = pg.display.set_mode((ANCHO, ALTO))
         self.botes = 0
-        self.cuentaGolpes = Marcador(10,10)
-
-        self.ballGroup = pg.sprite.Group()  # creamos un grupo de bolas
-        for i in range(random.randint(1, 10)):  # 10 bolas máximo
-            bola = Bola(random.randint(0, ANCHO), random.randint(0, ALTO))
-            self.ballGroup.add(bola)
-
-        # self.bola = Bola(ANCHO//2, ALTO//2)  # el atributo bola de la clase Game es a su vez una instancia de la clase Bola 
-                                               # (comentamos la línea xk ya definimos un grupo de bolas)
-
+        self.todoGrupo = pg.sprite.Group()  # creamos un grupo vacío
+        self.cuentaSegundos = Marcador(10,10)
+        self.todoGrupo.add(self.cuentaSegundos)
+        self.bola = Bola(random.randint(0, ANCHO), random.randint(0, ALTO))
+        self.todoGrupo.add(self.bola)
+        self.raqueta = Raqueta(x = ANCHO//2, y = ALTO - 40)
+        self.todoGrupo.add(self.raqueta)  
     def bucle_principal(self):
         game_over = False  # la variable game_over sólo se usará en este método por eso no le ponemos el self
         reloj = pg.time.Clock()
+        contador_milisegundos = 0
+        segundero = 0
         while not game_over:
-            reloj.tick(FPS)
-
+            dt = reloj.tick(FPS)
+            contador_milisegundos += dt
+            if contador_milisegundos >= 1000:
+                segundero += 1
+                contador_milisegundos = 0
             for evento in pg.event.get():
                 if evento.type == pg.QUIT:
                     game_over = True
-
-            # self.ball.update()
-            self.ballGroup.update()
-
+            self.cuentaSegundos.text = segundero
+            self.todoGrupo.update()
             self.pantalla.fill((0,0,0))
-            self.cuentaGolpes.dibuja('Hola', self.pantalla)
-
-            # self.pantalla.blit(self.bola.image, self.bola.rect.topleft)  # blit funciona con la esquina sup. izq.
-            self.ballGroup.draw(self.pantalla)
-
+            self.todoGrupo.draw(self.pantalla)
             pg.display.flip()
-
 
 if __name__ == '__main__':
     pg.init()
