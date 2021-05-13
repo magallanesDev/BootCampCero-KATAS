f1f7e11961c3b4354f5adf851a0a31a066f89835
Mario Filgueiras <magallanesdeveloper@gmail.com>
2021-05-14 00:45:21 +0200

    K03 - Ejemplo covid - final 13 MAY 2021

--- K03/covid_class/covid/views.py
+++ K03/covid_class/covid/views.py
@@ -1,5 +1,45 @@
 from covid import app
+import csv
+import json
 
-@app.route('/')
-def index():
-    return "Flask está funcionando desde views!!!"
+@app.route('/provincias')
+def provincias():
+    fichero = open('data/provincias.csv', 'r')
+    csvreader = csv.reader(fichero, delimiter=',')  # guarda en forma de lista cada registro
+
+    lista = []
+    for registro in csvreader:
+        d = {'codigo': registro[0], 'valor': registro[1]}
+        lista.append(d)  # será una lista de diccionarios
+
+    fichero.close()
+    print(lista)
+    return json.dumps(lista)  # transformamos la lista a formato json
+
+
+@app.route('/provincia/<codigoProvincia>')
+def laprovincia(codigoProvincia):
+    fichero = open('data/provincias.csv', 'r')
+    dictreader = csv.DictReader(fichero, fieldnames=['codigo', 'provincia'])
+    for registro in dictreader:
+        if registro['codigo'] == codigoProvincia:
+            fichero.close()
+            return registro['provincia']
+    
+    fichero.close()
+    return "La provincia no existe"
+
+
+#  @app.route('/casos/<int:year>' defaults={'mes:None', dia:None})
+#  @app.route('/casos/<int:year>/<int:mes>', defaults={'dia':None})
+
+@app.route('/casos/<int:year>/<int:mes>/<int:dia>')
+def casos(year, mes, dia):
+    pass
+
+'''
+Primer caso: Devolver el número total de casos covid en un día del año determinado para todas las provincias
+Segundo caso: Lo mismo pero detallado por tipo, PCR, AC, AG, ELISA, DESCONOCIDO -> JSON
+'''
+    
+
--- K03/covid_class/readme.md
+++ K03/covid_class/readme.md
@@ -9,3 +9,5 @@ Duplicar el fichero 'env_template' y renombrar a '.env'
 Los valores deben ser:
 - FLASK_APP=run.py
 - FLASK_ENV=el que queráis
+
+Ejecutar 'flask run'
