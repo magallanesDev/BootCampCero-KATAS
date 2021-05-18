aaebd7144c2f56308a7df0d255518aa9aa1bd8ed
Mario Filgueiras <magallanesdeveloper@gmail.com>
2021-05-19 00:49:09 +0200

    K03 - Ejemplo covid - final 18 MAY 2021

--- K03/covid_class/covid/views.py
+++ K03/covid_class/covid/views.py
@@ -2,6 +2,8 @@ from flask import render_template, request
 from covid import app
 import csv
 import json
+from datetime import date
+
 
 @app.route('/provincias')
 def provincias():
@@ -65,10 +67,37 @@ def casos(year, mes, dia):
     return json.dumps(res)
     
     
-@app.route('/incidenciadiaria', methods = ['GET', 'POST'])
+@app.route("/incidenciasdiarias", methods = ['GET', 'POST'])
 def incidencia():
+    formulario = {
+        'provincia': '',
+        'fecha': str(date.today()),
+        'num_casos_prueba_pcr': 0,
+        'num_casos_prueba_test_ac': 0, 
+        'num_casos_prueba_ag': 0,
+        'num_casos_prueba_elisa': 0,
+        'num_casos_prueba_desconocida': 0
+    }
+    fichero = open('data/provincias.csv', 'r')
+    csvreader = csv.reader(fichero, delimiter=",")
+    next(csvreader)
+    lista = []
+    for registro in csvreader:
+        d = {'codigo': registro[0], 'descripcion': registro[1]}
+        lista.append(d)
+    fichero.close()
     if request.method == 'GET':
-        return render_template('alta.html')
-    
-    return "Se ha hecho un post"
+        return render_template("alta.html", datos=formulario, 
+                               provincias=lista, error="")
+    for clave in formulario:
+        formulario[clave] = request.form[clave]
+    #validar que num_casos en general es no negativo
+    num_pcr = request.form['num_casos_prueba_pcr']
+    try:
+        num_pcr = int(num_pcr)
+        if num_pcr < 0:
+            raise ValueError('Debe ser no negativo')
+    except ValueError:
+        return render_template("alta.html", datos=formulario, error = "PCR no puede ser negativa")
+    return 'Ha hecho un post'
     
