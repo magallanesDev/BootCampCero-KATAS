b9cfc27c838e9a5b9c571f634d1d921f3736b0c3
Mario Filgueiras <magallanesdeveloper@gmail.com>
2021-05-21 00:47:00 +0200

    K03 - kakebo - inicio SQLite - final 20 MAY 2021

--- K03/kakebo/views.py
+++ K03/kakebo/views.py
@@ -1,5 +1,26 @@
 from kakebo import app
+import sqlite3
 
 @app.route('/')
 def index():
-    return 'Flask rulando'
+    conexion = sqlite3.connect('movimientos.db')
+    cur = conexion.cursor()
+
+    cur.execute('SELECT * FROM movimientos;')  # las sentencias SQL se acaban en ;
+
+    claves = cur.description
+
+    filas = cur.fetchall()
+
+    '''
+    l = []  # creamos una lista vacía. Despúes será una lista de diccionarios
+    for fila in filas:
+        d = {}  # creamos un diccionario vacío
+        for columna in fila:
+            # usaremos enumarate o un índice
+            # lo pasaremos a json la lista de diccionarios
+    '''
+        
+    conexion.close()
+
+    return 'Consulta realizada'
