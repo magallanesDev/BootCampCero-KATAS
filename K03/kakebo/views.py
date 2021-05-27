b468431ee32e0621e5f387250ce346461de7ca31
Mario Filgueiras <magallanesdeveloper@gmail.com>
2021-05-27 22:44:17 +0200

    K03 - Refactorizando acceso a BD y añadiendo borrar

--- K03/kakebo/views.py
+++ K03/kakebo/views.py
@@ -1,32 +1,56 @@
 from kakebo import app
-from flask import jsonify, render_template, request, redirect, url_for
+from flask import jsonify, render_template, request, redirect, url_for, flash
 from kakebo.forms import MovimientosForm
 import sqlite3
 
-@app.route('/')
-def index():
+def consultaSQL(query):
+    # Abrimos la conexión
     conexion = sqlite3.connect('movimientos.db')
     cur = conexion.cursor()
 
-    cur.execute('SELECT * FROM movimientos;')  # las sentencias SQL se acaban en ;
+    # Ejecutamos la consulta
+    cur.execute(query)
 
+    # Obtenemos los datos de la consulta
     claves = cur.description
     filas = cur.fetchall()
 
-    movimientos = []  # creamos una lista vacía. Despúes será una lista de diccionarios
-    saldo = 0
+    # Procesar los datos para devolver una lista de dicionarios. Um diccionario por fila
+    resultado = []  # creamos una lista vacía. Despúes será una lista de diccionarios
     for fila in filas:
         d ={}  # creamos un diccionario vacío
         for tuplaClave, valor in zip(claves, fila):
             d[tuplaClave[0]] = valor
+        
+        resultado.append(d)
+
+    conexion.close()
+    return resultado
+
+
+
+def modificaTablaSQL(query, parametros=[]):
+    conexion = sqlite3.connect('movimientos.db')
+    cur = conexion.cursor()
+
+    cur.execute(query, parametros)
+
+    conexion.commit()
+    conexion.close()
+    
+
+
+@app.route('/')
+def index():
+    movimientos = consultaSQL("SELECT * FROM movimientos order by fecha")
+
+    saldo = 0
+    for d in movimientos:
         if d['esGasto'] == 0:
             saldo = saldo + d['cantidad']
         else:
             saldo = saldo - d['cantidad']
-        d['saldo'] = saldo
-        movimientos.append(d)  
-
-    conexion.close()
+        d['saldo'] = saldo 
 
     # return jsonify(movimientos)
     return render_template('movimientos.html', datos = movimientos)
@@ -39,21 +63,22 @@ def nuevo():
         return render_template('alta.html', form = formulario)
     else:  # será un POST
         if formulario.validate():  # insertar el movimiento en la base de datos
-            conexion = sqlite3.connect('movimientos.db')
-            cur = conexion.cursor()
 
             query = """ INSERT INTO movimientos (fecha, concepto, categoria, esGasto, cantidad)
                         VALUES (?, ?, ?, ?, ?) """
             try:
-                cur.execute(query, [formulario.fecha.data, formulario.concepto.data, formulario.categoria.data,
+                modificaTablaSQL(query, [formulario.fecha.data, formulario.concepto.data, formulario.categoria.data,
                                     formulario.esGasto.data, formulario.cantidad.data])
             except sqlite3.Error as el_error:
                 print("ERROR EN SQL", el_error)
+                flash('Se ha producido un error en la base de datos. Pruebe en unos minutos')
                 return render_template('alta.html', form = formulario)
-            
-            conexion.commit()  # fijamos el movimiento en la base de datos
-            conexion.close()
 
             return redirect(url_for('index'))  # redirect a la ruta / a través de la función index
         else:
             return render_template('alta.html', form = formulario)
+
+
+@app.route('/borrar/<int:id>', methods=['GET', 'POST'])
+def borrar(id):
+    return render_template('borrar.html', identificador = id)
