60c4b4d8d220b7547746b534ece581e337bd18b8
Mario Filgueiras <magallanesdeveloper@gmail.com>
2021-06-04 00:27:06 +0200

    K05 - Silabeador OK

--- K05/silabeador/views.py
+++ K05/silabeador/views.py
@@ -1,5 +1,10 @@
 from silabeador import app
+from silabeador.tools import pilengua
+from flask import jsonify
 
 @app.route("/pilengua/<frase>")
-def pilengua(frase):
-    return "Has escrito " + frase
+def enlenguapi(frase):
+    respuesta = pilengua(frase)
+    d = { 'original' : frase,
+        'pilengua' : respuesta }
+    return jsonify(d)
