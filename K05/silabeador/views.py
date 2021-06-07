625da89af66b5792d358243929ca0d986c97ccdb
Mario Filgueiras <magallanesdeveloper@gmail.com>
2021-06-08 00:43:06 +0200

    K05 - Silabeador con ruta buscador de pelis

--- K05/silabeador/views.py
+++ K05/silabeador/views.py
@@ -1,6 +1,7 @@
 from silabeador import app
 from silabeador.tools import pilengua
 from flask import jsonify
+import requests
 
 @app.route("/pilengua/<frase>")
 def enlenguapi(frase):
@@ -8,3 +9,16 @@ def enlenguapi(frase):
     d = { 'original' : frase,
         'pilengua' : respuesta }
     return jsonify(d)
+
+
+@app.route("/pelicula/<palabra>")
+def pelicula(palabra):
+    url = "http://www.omdbapi.com/?s={}&apikey=2bf0cc92"
+
+    resultado = requests.get(url.format(palabra))
+    if resultado.status_code == 200:
+        peliculas = resultado.json()
+        if peliculas['Response'] == "False":
+            return jsonify({'status': "Error", "msg": "No se han encontrado resultados"})
+
+        return jsonify({"Películas": peliculas["Search"], 'status': "Success"})
